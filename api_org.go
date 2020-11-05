f93e83cfc8bf5e257bbafd708e0b1af3030517d3
storezhang <storezhang@gmail.com>
2020-11-05 13:35:04 +0800

    YUNKE-1781 【Arch】支持Open API鉴权

--- api_org.go
+++ api_org.go
@@ -5,6 +5,8 @@ const (
 	OrgApiClientPackageNotifyUrl string = "clients/{id}/packages/notifies"
 	// OrgApiClientAddUrl 添加客户端地址
 	OrgApiClientAddUrl string = "clients"
+	// OrgApiClientGetById 按编号获得客户端信息
+	OrgApiClientGetById string = "clients/{id}"
 
 	// OrgApiConfigUpdateUrl 更新配置地址
 	OrgApiConfigUpdateUrl string = "configs/{name}"
