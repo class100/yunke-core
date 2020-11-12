d1d4f278f03f6203293de91de46963517989018a
i9code <48942204+i9code@users.noreply.github.com>
2020-11-12 13:39:31 +0800

    YUNKE-1907 【Server】给330提供大文件上传接口

---
+++ api_file_chunk.go
@@ -0,0 +1,10 @@
+package core
+
+const (
+	// FileApiInitiateMultipartUpload 初始化分块上传信息
+	FileApiInitiateMultipartUpload ApiPath = "/uploads/multiparts/chunks"
+	// FileApiCompleteMultipartUpload 完成化分块上传信息
+	FileApiCompleteMultipartUpload ApiPath = "/uploads/multiparts/chunks"
+	// FileApiAbortMultipartUploadReq 取消分块上传
+	FileApiAbortMultipartUploadReq ApiPath = "/uploads/multiparts/chunks"
+)
