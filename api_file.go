7e4bb625fdc2794432868972761cb2ecade4ca51
i9code <48942204+i9code@users.noreply.github.com>
2021-01-06 17:41:16 +0800

    YUNKE-2418 【Server】调整yunke-core,yunke文件上传接口，为330提供接口

--- api_file.go
+++ api_file.go
@@ -2,9 +2,11 @@ package core
 
 const (
 	// FileApiUploadGet 获取文件上传信息
-	FileApiUploadGet ApiPath = "courses/resources/uploads/infos"
+	FileApiUploadGet ApiPath = "files/uploads/infos"
 	// FileApiDownloadGet 获取文件下载,打开信息
-	FileApiDownloadGet ApiPath = "courses/resources/{fileId}/downloads"
+	FileApiDownloadGet ApiPath = "files/{fileId}/downloads/infos"
 	// FileApiDelete 根据文件编号删除
-	FileApiDelete ApiPath = "courses/resources/{fileId}"
+	FileApiDelete ApiPath = "files/{fileId}"
+	// FileApiCompletedUploadNotice 完成上传通知服务器上传成功
+	FileApiCompletedUploadNotice = "files/{fileId}/notifies"
 )
