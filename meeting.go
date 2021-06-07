02f5b8cf582ba601a898aaa28e7b8c5d1cb98b41
i9code <48942204+i9code@users.noreply.github.com>
2021-06-07 16:21:07 +0800

    YUNKE-3862 【Server】调整互动课直播sdk为腾讯云

--- meeting.go
+++ meeting.go
@@ -24,21 +24,16 @@ type (
 		VirtualUserId string `json:"virtualUserId"`
 		// MeetingToken 令牌
 		MeetingToken string `json:"meetingToken"`
-		// VirtualPhone 虚拟手机号 TODO ,后期删除
-		VirtualPhone string `json:"virtual_phone"`
 		// MeetingPhone 会议中手机号
 		MeetingPhone string `json:"meetingPhone"`
 		// MeetingId 会议Id
 		MeetingId string `json:"meetingId"`
 		// MeetingNo 会议编号
-		MeetingNo uint64 `json:"meetingNo"`
+		MeetingNo uint64 `json:"meetingNo,string"`
 		// GroupId 组Id
 		GroupId int64 `json:"groupId,string"`
 		// GroupWebsocket 组websocket地址
 		GroupWebsocket string `json:"groupWebsocket"`
-		// TODO ①以后删除掉,后也删除
-		// UserToken 当请求接口中 RoleType为1时有值
-		UserToken string `json:"userToken"`
 	}
 
 	// LeaveMeetingReq 离开会议请求
